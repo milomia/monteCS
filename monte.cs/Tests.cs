12145ebc0368a0f1382c06423487136f28796301
BuildTools <unconfigured@null.spigotmc.org>
2017-01-29 10:13:06 +0000

    more tests

--- monte.cs/Tests.cs
+++ monte.cs/Tests.cs
@@ -6,12 +6,13 @@ namespace monte.cs
     [TestClass]
     public class testMonte
     {
+        [TestMethod]
         public void testGausian()
         { // push the changes
           // push the changes downstairs
             Monte mc = new Monte();
             double g = mc.gaussian_box_muller();
-            Console.WriteLine(g);
+            Assert.IsTrue(g < 0);
         }
     }
 }
